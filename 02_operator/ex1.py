b774cf1d4be151529ad4948b058b392b5735ef8d
parkjihoonn <parkjihoon@dimigo.hs.kr>
2026-08-28 14:32:31 +0900

    1530

---
+++ 02_operator/ex1.py
@@ -0,0 +1,45 @@
+#산술연산자
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #나머지
+print(a // b) #몫
+print(a ** b) #거듭제곱
+
+#복합대입연산자
+a += 4
+print(a)
+
+a-= 10
+print(a)
+
+#증감연산자
+b = a++
+
+a += 1
+
+#비교연산자
+print(3 == 3.0)
+print(3 != 3.0)
+print("apple" < "apble")
+print(1 < 2 < 3) #1<2 and 2<3
+
+#논리연산자
+print(True and False)
+print(True or False)
+print(not True)
+
+#short-circuit test
+a = 10
+b = 0
+#print(a / b)
+
+if a > 0 or a / b;
+    print(yes)
+else:
+    print(no)
+
